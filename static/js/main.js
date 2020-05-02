d180f15ac8342440090decc20b36a99a24a7af35
aravind-reddy-dandu <aravindreddy444@gmail.com>
2020-05-01 21:42:51 -0400

    Clean up

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-queue()
-        .defer(d3.json, piechartDataUrl)
-        .defer(d3.json, barchartDataUrl)
-        .await(ready);
-
-function ready(error, dataset, datasetBarChart) {
-    d3PieChart(dataset, datasetBarChart);
-    d3BarChart(datasetBarChart);
-    }
